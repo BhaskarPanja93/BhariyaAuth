1e71863af3d5f10c1f8fa87acbb5f286cdc73bca
Riya <riyapuri0710@gmail.com>
2025-11-29 15:37:55 +0530

    otp input prime

--- Frontend/src/AuthFlow/Common/OTPInput.jsx
+++ Frontend/src/AuthFlow/Common/OTPInput.jsx
@@ -1,10 +1,38 @@
+import React from 'react';
 import { InputOtp } from 'primereact/inputotp';
 
-export default function OTPInput({value, onValueChange, disabled}) {
+export default function OTPInput({ value, onValueChange, disabled }) {
+    const customInput = ({ events, props }) => (
+        <input
+            {...events}
+            {...props}
+            type="text"
+            className="
+                w-12 h-12
+                text-center
+                rounded-md
+                bg-[#0b0f14]
+                border border-gray-700
+                text-white
+                text-lg
+                tracking-widest
+                focus:outline-none
+                focus:ring-2
+                focus:ring-indigo-500
+            "
+        />
+    );
 
     return (
-        <div className="flex justify-between gap-2 mt-2">
-            <InputOtp value={value} onChange={(e) => onValueChange(e.value)} integerOnly disabled={disabled} length={6} />
+        <div className="flex gap-2 mt-2">
+            <InputOtp
+                value={value}
+                onChange={(e) => onValueChange(e.value)}
+                integerOnly
+                disabled={disabled}
+                length={6}
+                inputTemplate={customInput}
+            />
         </div>
     );
 }
