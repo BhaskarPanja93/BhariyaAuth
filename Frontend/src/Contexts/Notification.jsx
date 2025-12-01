0df5ed50cd81bbade7ea86aa01de8480b51beaf4
Riya <riyapuri0710@gmail.com>
2025-12-01 21:48:21 +0530

    Frontend notification color

--- Frontend/src/Contexts/Notification.jsx
+++ Frontend/src/Contexts/Notification.jsx
@@ -36,7 +36,7 @@ export default function NotificationProvider({children}) {
              style={{zIndex: 50}}>
             {notifications.map((notification) => (
                 <div key={notification.id}
-                     style={{border: "4px solid #ffc300"}}
+                     style={{border: "4px solid #a855f7"}}
                      className="bg-white border shadow-xl rounded-xl px-4 py-2 text-gray-800 transition-opacity duration-300">
                     {notification.message}
                 </div>)
