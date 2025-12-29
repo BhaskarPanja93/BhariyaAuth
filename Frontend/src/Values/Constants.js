63b1a5faf5fa41d51ff2f4e451eb2f3492921a10
Bhaskar Panja <bhaskarpanja93@gmail.com>
2025-12-29 05:58:41 +0530

    // Changed cookie names to be read by the frontend (missed in previous commit)
    FUTURE:
    // shorten log texts.
    // offload logs storage to external custom servers.
    // prevent mfa based activities immediately if session was revoked.

--- Frontend/src/Values/Constants.js
+++ Frontend/src/Values/Constants.js
@@ -4,5 +4,5 @@ export const FrontendDomain = BackendDomain
 export const BackendURL = BackendDomain + "/auth-backend";
 export const FrontendURL = FrontendDomain + "/auth";
 
-export const CSRFCookiePath = "bhariya_auth_csrf"
-export const MFACookiePath = "bhariya_auth_mfa"
+export const CSRFCookiePath = "csrf"
+export const MFACookiePath = "mfa"
