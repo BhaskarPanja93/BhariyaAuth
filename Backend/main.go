25ba729889da21a8b47c3c92e9a6fe5acf3ebf6d
Bhaskar <bhaskarpanja93@gmail.com>
2026-03-09 21:13:10 +0530

    // Bumped go package versions // Made the app trust proxy (Fiber now properly allows unix domain sockets as proxy)

--- Backend/main.go
+++ Backend/main.go
@@ -4,6 +4,7 @@ import (
 	Middlewares "BhariyaAuth/middlewares"
 	AccountProcessor "BhariyaAuth/processors/account"
 	AccountRouters "BhariyaAuth/routers/account"
+	ChatRouters "BhariyaAuth/routers/chat"
 	LoginRouters "BhariyaAuth/routers/login"
 	MFARouters "BhariyaAuth/routers/mfa"
 	PasswordResetRouters "BhariyaAuth/routers/passwordreset"
@@ -50,7 +51,7 @@ func StartOnPort(MainApp *fiber.App) {
 }
 
 func main() {
-	Stores.ConnectMySQL()
+	Stores.ConnectSQL()
 	Stores.ConnectRedis()
 	go AccountProcessor.ServeAccountDetails()
 	go AccountProcessor.DatabaseAutoVacuum()
@@ -58,28 +59,31 @@ func main() {
 	MainApp := fiber.New(fiber.Config{
 		AppName:          "BhariyaAuth",
 		ProxyHeader:      fiber.HeaderXForwardedFor,
-		ReadBufferSize:   4 * 1024,
-		WriteBufferSize:  4 * 1024,
+		ReadBufferSize:   1024,
+		WriteBufferSize:  1024,
 		ReadTimeout:      30 * time.Second,
 		WriteTimeout:     30 * time.Second,
 		BodyLimit:        10 * 1024,
+		TrustProxy:       true,
 		TrustProxyConfig: fiber.TrustProxyConfig{Loopback: true},
 		JSONEncoder:      json.Marshal,
 		JSONDecoder:      json.Unmarshal,
 	})
 
-	MainApp.Use(Middlewares.ProfilingMiddleware())
+	APIGroup := MainApp.Group("/auth/api")
+	APIGroup.Use(Middlewares.ProfilingMiddleware())
 
-	AuthApp := MainApp.Group("/auth-backend")
+	WSGroup := MainApp.Group("/auth/ws")
 
-	StatusRouters.AttachRoutes(AuthApp)
-	AccountRouters.AttachRoutes(AuthApp)
-	PasswordResetRouters.AttachRoutes(AuthApp)
-	RegisterRouters.AttachRoutes(AuthApp)
-	LoginRouters.AttachRoutes(AuthApp)
-	SSORouters.AttachRoutes(AuthApp)
-	SessionRouters.AttachRoutes(AuthApp)
-	MFARouters.AttachRoutes(AuthApp)
+	StatusRouters.AttachRoutes(APIGroup)
+	AccountRouters.AttachRoutes(APIGroup)
+	PasswordResetRouters.AttachRoutes(APIGroup)
+	RegisterRouters.AttachRoutes(APIGroup)
+	LoginRouters.AttachRoutes(APIGroup)
+	SSORouters.AttachRoutes(APIGroup)
+	SessionRouters.AttachRoutes(APIGroup)
+	MFARouters.AttachRoutes(APIGroup)
+	ChatRouters.AttachRoutes(WSGroup)
 
 	ReceiveCLIFlags(MainApp)
 }
