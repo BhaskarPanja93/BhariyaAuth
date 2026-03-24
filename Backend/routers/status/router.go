c2912eba98cbffbf7f9269e53b0e8d5c5911b531
Bhaskar <bhaskarpanja93@gmail.com>
2026-03-24 14:15:24 +0530

    // Added /ip route that sends the "visible" and other IP addresses of the request.

--- Backend/routers/status/router.go
+++ Backend/routers/status/router.go
@@ -1,6 +1,7 @@
 package status
 
 import (
+	"strings"
 	"time"
 
 	"github.com/gofiber/fiber/v3"
@@ -8,9 +9,10 @@ import (
 	"github.com/gofiber/contrib/monitor"
 )
 
-func AttachRoutes(authApp fiber.Router) {
-	StatusRouter := authApp.Group("/status")
+func AttachRoutes(APIGroup fiber.Router) {
+	StatusRouter := APIGroup.Group("/status")
 
 	StatusRouter.Get("/metrics", monitor.New(monitor.Config{Title: "BhariyaAuth Metrics", Refresh: 1 * time.Second}))
 	StatusRouter.All("/ping", func(ctx fiber.Ctx) error { return nil })
+	StatusRouter.Get("/ip", func(ctx fiber.Ctx) error { return ctx.SendString(ctx.IP() + " <- " + strings.Join(ctx.IPs(), "/")) })
 }
