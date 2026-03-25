c1d7b61f80755ca5b7a1097013b1da22da682c22
Bhaskar <bhaskarpanja93@gmail.com>
2026-03-25 09:22:24 +0530

    // Modified ratelimiter values based on new RateLimiter requirements. // Using database transactions for refresh processing, now the device row will be locked until refresh completes, this will prevent overwriting in case of concurrent requests. // Refresh and Logout handlers Documented entirely

--- Backend/routers/account/handler.go
+++ Backend/routers/account/handler.go
@@ -1,88 +1,123 @@
 package account
 
 import (
+	Config "BhariyaAuth/constants/config"
+	Notifications "BhariyaAuth/models/notifications"
 	ResponseModels "BhariyaAuth/models/responses"
 	AccountProcessor "BhariyaAuth/processors/account"
-	Logger "BhariyaAuth/processors/logs"
+	CookieProcessor "BhariyaAuth/processors/cookies"
 	RateLimitProcessor "BhariyaAuth/processors/ratelimit"
-	ResponseProcessor "BhariyaAuth/processors/response"
 	TokenProcessor "BhariyaAuth/processors/token"
 	Stores "BhariyaAuth/stores"
-	"fmt"
+	"database/sql"
+	"errors"
 
 	"time"
 
 	"github.com/gofiber/fiber/v3"
 )
 
-func ProcessRefresh(ctx fiber.Ctx) error {
-	now := ctx.Locals("request-start").(time.Time)
+func Logout(ctx fiber.Ctx) error {
 	refresh, ok := TokenProcessor.ReadRefreshToken(ctx)
 	if !ok || !TokenProcessor.MatchCSRF(ctx, refresh) {
-		RateLimitProcessor.Set(ctx)
 		return ctx.SendStatus(fiber.StatusUnprocessableEntity)
 	}
-	if now.After(refresh.RefreshExpiry) || !AccountProcessor.CheckSessionExists(refresh.UserID, refresh.RefreshID) {
-		Logger.IntentionalFailure(fmt.Sprintf("[ProcessRefresh] Expired/Revoked session [UID-%d-RID-%d]", refresh.UserID, refresh.RefreshID))
-		RateLimitProcessor.Set(ctx)
-		ResponseProcessor.DetachAuthCookies(ctx)
-		ResponseProcessor.DetachMFACookies(ctx)
-		ResponseProcessor.DetachSSOCookies(ctx)
-		return ctx.Status(fiber.StatusUnauthorized).JSON(ResponseModels.APIResponseT{
-			Notifications: []string{"This session has expired..."},
+	CookieProcessor.DetachAuthCookies(ctx)
+	CookieProcessor.DetachMFACookies(ctx)
+	CookieProcessor.DetachSSOCookies(ctx)
+	AccountProcessor.DeleteSession(refresh.UserID, refresh.DeviceID)
+	return ctx.Status(fiber.StatusOK).JSON(
+		ResponseModels.APIResponseT{
+			Success:    true,
+			ModifyAuth: true,
 		})
-	}
-	var currentIndex uint16
-	err := Stores.MySQLClient.QueryRow("SELECT count FROM activities WHERE refresh = ? AND uid = ?", refresh.RefreshID, refresh.UserID).Scan(&currentIndex)
-	if err != nil {
-		Logger.AccidentalFailure(fmt.Sprintf("[ProcessRefresh] Unable to fetch count for [UID-%d-RID-%d] reason: %s", refresh.UserID, refresh.RefreshID, err.Error()))
-		return ctx.Status(fiber.StatusInternalServerError).JSON(ResponseModels.APIResponseT{
-			Notifications: []string{"Failed to acquire session (DB-read issue)... Retrying"},
-		})
-	}
-	if currentIndex != refresh.RefreshIndex {
-		Logger.IntentionalFailure(fmt.Sprintf("[ProcessRefresh] Used old index [UID-%d-RID-%d]", refresh.UserID, refresh.RefreshID))
-		ResponseProcessor.DetachAuthCookies(ctx)
-		ResponseProcessor.DetachMFACookies(ctx)
-		ResponseProcessor.DetachSSOCookies(ctx)
-		RateLimitProcessor.Set(ctx)
-		return ctx.SendStatus(fiber.StatusUnprocessableEntity)
-	}
-	token, ok := TokenProcessor.CreateRenewToken(refresh, ctx)
-	if !ok {
-		return ctx.Status(fiber.StatusInternalServerError).JSON(ResponseModels.APIResponseT{
-			Notifications: []string{"Failed to acquire session (Encryptor issue)... Retrying"},
-		})
-	}
-	_, err = Stores.MySQLClient.Exec("UPDATE activities SET count = ?, updated = ? WHERE refresh = ? AND uid = ?", refresh.RefreshIndex+1, now, refresh.RefreshID, refresh.UserID)
-	if err != nil {
-		Logger.AccidentalFailure(fmt.Sprintf("[ProcessRefresh] Failed to update count for [UID-%d-RID-%d] reason: %s", refresh.UserID, refresh.RefreshID, err.Error()))
-		return ctx.Status(fiber.StatusInternalServerError).JSON(ResponseModels.APIResponseT{
-			Notifications: []string{"Failed to acquire session (DB-write issue)... Retrying"},
-		})
-	}
-	ResponseProcessor.AttachAuthCookies(ctx, token)
-	Logger.Success(fmt.Sprintf("[ProcessRefresh] Success for [UID-%d-RID-%d]", refresh.UserID, refresh.RefreshID))
-	return ctx.Status(fiber.StatusOK).JSON(ResponseModels.APIResponseT{
-		Success:    true,
-		ModifyAuth: true,
-		NewToken:   token.AccessToken,
-		Reply:      token.AccessExpires.Format(time.RFC3339),
-	})
 }
 
-func ProcessLogout(ctx fiber.Ctx) error {
+func Refresh(ctx fiber.Ctx) error {
+	now := ctx.Locals("request-start").(time.Time)
 	refresh, ok := TokenProcessor.ReadRefreshToken(ctx)
+	// Refresh requires CSRF
 	if !ok || !TokenProcessor.MatchCSRF(ctx, refresh) {
-		RateLimitProcessor.Set(ctx)
-		return ctx.SendStatus(fiber.StatusUnprocessableEntity)
+		RateLimitProcessor.Add(ctx, 10_000) // 60 mistakes allowed / minute
+		return ctx.SendStatus(fiber.StatusUnauthorized)
 	}
-	ResponseProcessor.DetachAuthCookies(ctx)
-	ResponseProcessor.DetachMFACookies(ctx)
-	ResponseProcessor.DetachSSOCookies(ctx)
-	AccountProcessor.DeleteSession(refresh.UserID, refresh.RefreshID)
-	return ctx.Status(fiber.StatusOK).JSON(ResponseModels.APIResponseT{
-		Success:    true,
-		ModifyAuth: true,
-	})
+	// Expiry should be in the future for a refresh token to be called active
+	if now.After(refresh.Expiry) {
+		RateLimitProcessor.Add(ctx, 10_000) // 60 mistakes allowed / minute
+		return ctx.Status(fiber.StatusUnauthorized).JSON(
+			ResponseModels.APIResponseT{
+				Notifications: []string{Notifications.SessionExpired},
+			})
+	}
+	// Initiate a transaction because 2 separate (read and write) actions will be performed internally
+	// and concurrency may cause overwriting
+	tx, err := Stores.SQLClient.Begin(Config.CtxBG)
+	if err != nil {
+		RateLimitProcessor.Add(ctx, 10_000) // 60 mistakes allowed / minute
+		return ctx.Status(fiber.StatusInternalServerError).JSON(
+			ResponseModels.APIResponseT{
+				Notifications: []string{Notifications.DBReadError},
+			})
+	}
+	defer tx.Rollback(Config.CtxBG)
+	// Fetch the visits column which in other words mean the most recent version of refresh token for that specific device id
+	var visits int16
+	err = tx.QueryRow(Config.CtxBG, "SELECT visits FROM devices where user_id = $1 AND device_id = $2 LIMIT 1 FOR UPDATE", refresh.UserID, refresh.DeviceID).Scan(&visits)
+	if errors.Is(err, sql.ErrNoRows) { // Row(device) not found
+		RateLimitProcessor.Add(ctx, 10_000) // 60 mistakes allowed / minute
+		return ctx.Status(fiber.StatusUnauthorized).JSON(
+			ResponseModels.APIResponseT{
+				Notifications: []string{Notifications.SessionRevoked},
+			})
+	} else if err != nil { // Any other DB error
+		RateLimitProcessor.Add(ctx, 1_000) // 600 mistakes allowed / minute
+		return ctx.Status(fiber.StatusInternalServerError).JSON(
+			ResponseModels.APIResponseT{
+				Notifications: []string{Notifications.DBReadError},
+			})
+	}
+	// DB visits should match refresh visits
+	if visits != refresh.Visits {
+		RateLimitProcessor.Add(ctx, 10_000) // 60 mistakes allowed / minute
+		return ctx.Status(fiber.StatusUnauthorized).JSON(
+			ResponseModels.APIResponseT{
+				Notifications: []string{Notifications.SessionRevoked},
+			})
+	}
+	// Create their access and refresh tokens
+	token, ok := TokenProcessor.CreateFreshToken(ctx, refresh.UserID, refresh.DeviceID, refresh.UserType, refresh.RememberMe, "email-login")
+	if !ok {
+		RateLimitProcessor.Add(ctx, 10_000) // 60 mistakes allowed / minute
+		return ctx.Status(fiber.StatusInternalServerError).JSON(ResponseModels.APIResponseT{
+			Notifications: []string{Notifications.UnknownError},
+		})
+	}
+	// Increment the visit count in database, which will in turn auto-revoke all old versions of the refresh token
+	_, err = tx.Exec(Config.CtxBG, "UPDATE devices SET visits = $1, updated = $2 WHERE device_id = $3 AND user_id = $4", refresh.Visits+1, now, refresh.DeviceID, refresh.UserID)
+	if err != nil {
+		RateLimitProcessor.Add(ctx, 10_000) // 60 mistakes allowed / minute
+		return ctx.Status(fiber.StatusInternalServerError).JSON(
+			ResponseModels.APIResponseT{
+				Notifications: []string{Notifications.DBWriteError},
+			})
+	}
+	// Release the row for future access
+	err = tx.Commit(Config.CtxBG)
+	if err != nil {
+		RateLimitProcessor.Add(ctx, 10_000) // 60 mistakes allowed / minute
+		return ctx.Status(fiber.StatusInternalServerError).JSON(
+			ResponseModels.APIResponseT{
+				Notifications: []string{Notifications.DBWriteError},
+			})
+	}
+	// Attach the new refresh token and CSRF as cookie
+	CookieProcessor.AttachAuthCookies(ctx, token)
+	// Return the new access token and its expiry
+	return ctx.Status(fiber.StatusOK).JSON(
+		ResponseModels.APIResponseT{
+			Success:    true,
+			ModifyAuth: true,
+			NewToken:   token.AccessToken,
+			Reply:      token.AccessExpires,
+		})
 }
--- Backend/routers/account/router.go
+++ Backend/routers/account/router.go
@@ -7,9 +7,8 @@ import (
 	"github.com/gofiber/fiber/v3"
 )
 
-func AttachRoutes(authApp fiber.Router) {
-	AccountRouter := authApp.Group("/account")
-
-	AccountRouter.Post("/logout", ProcessLogout)
-	AccountRouter.Post("/refresh", Middlewares.RouteRateLimiter(60, time.Minute, 3*time.Minute, 10*time.Minute), ProcessRefresh)
+func AttachRoutes(APIGroup fiber.Router) {
+	AccountRouter := APIGroup.Group("/account")
+	AccountRouter.Post("/logout", Logout)
+	AccountRouter.Post("/refresh", Middlewares.RouteRateLimiter(60_000, time.Minute, 2*time.Minute), Refresh)
 }
