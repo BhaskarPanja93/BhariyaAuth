95f1528b9afd79528ff42a9def84dca3722b791e
Bhaskar <bhaskarpanja93@gmail.com>
2026-04-06 17:16:39 +0530

    // Added backend and frontend connectivity based feature flags. To manually enable and disable if frontend or backend or websocket is in Beta.
    // Max sessions per user has been moved to database instead og golang.
    
    // SSO callback route now separates provider based on URL `/sso/callback` changed to `/sso/callback/:provider` where provider is a valid goth provider.

--- Backend/constants/config/main.go
+++ Backend/constants/config/main.go
@@ -1,19 +1,24 @@
-package config
+package Config
 
 import (
-	"fmt"
+	"context"
 	"time"
 )
 
-var (
-	ServerFor            = "bhariya"
-	ServerRole           = "auth"
-	CookieDomain         = "bhariya.ddns.net"
-	FrontendURL          = fmt.Sprintf("https://%s/auth", CookieDomain)
-	ServerSSOCallbackURL = fmt.Sprintf("https://%s/auth-backend/sso/callback", CookieDomain)
+const (
+	BetaFrontend = false
+	BetaAPI      = false
+	BetaWS       = false
 )
 
-var (
+const (
+	Domain      = "bhariya.ddns.net"
+	Origin      = "https://" + Domain
+	Purpose     = "/auth"
+	PurposeFull = Origin + Purpose
+)
+
+const (
 	CSRFInHeader         = "csrf"
 	MFATokenInHeader     = "mfa"
 	CSRFInCookie         = CSRFInHeader
@@ -26,12 +31,43 @@ var (
 	RefreshTokenExpireDelta = time.Hour * 24 * 7
 	SSOCookieExpireDelta    = time.Minute * 10
 
-	RedisServerBase            = fmt.Sprintf("%s:%s", ServerFor, ServerRole)
-	RedisServerOTPVerification = fmt.Sprintf("%s:verify", RedisServerBase)
-
 	RefreshBlocked                = "auth:blocked"
-	AccountDetailsRequestChannel  = "auth:account:request"
-	AccountDetailsResponseChannel = "auth:account:response"
+	AccountDetailsRequestChannel  = "auth:acc:req"
+	AccountDetailsResponseChannel = "auth:acc:res"
 
-	MaxUserSessions = 50
+	ServerID = "auth"
+)
+
+var (
+	FrontendPrefix = ""
+	FrontendSuffix = func() string {
+		if BetaFrontend {
+			return "/beta"
+		}
+		return ""
+	}()
+
+	APIPrefix = "/api"
+	APISuffix = func() string {
+		if BetaAPI {
+			return "/beta"
+		}
+		return ""
+	}()
+
+	WSPrefix = "/ws"
+	WSSuffix = func() string {
+		if BetaWS {
+			return "/beta"
+		}
+		return ""
+	}()
+
+	FrontendRoute = PurposeFull + FrontendPrefix + FrontendSuffix
+	APIRoute      = PurposeFull + APIPrefix + APISuffix
+	WSRoute       = PurposeFull + WSPrefix + WSSuffix
+
+	ServerSSOCallbackURL = APIRoute + "/sso/callback"
+
+	CtxBG = context.Background()
 )
