3c71d340f912a5bc9f336b9ce2d30dd200f0efd4
Bhaskar <bhaskarpanja93@gmail.com>
2026-05-08 01:51:48 +0530

    // Renamed Log to Logs and added to main app router

--- Frontend/src/Structures/Router.tsx
+++ Frontend/src/Structures/Router.tsx
@@ -5,7 +5,7 @@ const RegisterStructure = lazy(() => import('../Structures/SignUp'))
 const SessionsStructure = lazy(() => import('../Structures/Sessions'))
 const PasswordResetStructure = lazy(() => import('../Structures/PasswordReset'))
 const MfaStructure = lazy(() => import('../Structures/Mfa'))
-const LogStructure = lazy(() => import('../Structures/Log'))
+const LogsStructure = lazy(() => import('./Logs'))
 
 import '../index.css'
 
@@ -18,7 +18,7 @@ export default function Router() {
                 <Route path="/sessions" element={<SessionsStructure/>}/>
                 <Route path="/mfa" element={<MfaStructure/>}/>
                 <Route path="/passwordreset" element={<PasswordResetStructure/>}/>
-                <Route path="/log" element={<LogStructure/>}/>
+                <Route path="/logs" element={<LogsStructure/>}/>
                 <Route path="*" element={<Navigate to="/sessions" replace/>}/>
             </Routes>
         </Suspense>
